6eb02c7560501a0bb62544270929c270e2a90d13
omosnacek@gmail.com <omosnacek@gmail.com@24eb42d8-d958-32f7-5ae2-b5831bfd505f>
2014-05-13 12:51:09 +0000

    Implemented comparing of node lists when order does not matter. Added some unit tests for the Node*Wrapper classes that I forgot to commit earlier.

---
+++ trunk/Soxc/test/cz/muni/fi/courses/pb138/j2014/projects/soxc/NodeWrappersTests.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.muni.fi.courses.pb138.j2014.projects.soxc;
+
+import java.io.IOException;
+import java.io.StringReader;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ * Some tests for the Node*Wrapper classes.
+ * 
+ * @author Ondrej Mosnacek <omosnacek@gmail.com>
+ */
+public class NodeWrappersTests {
+    
+    private DocumentBuilder docBuilder;
+
+    @Before
+    public void setUp() throws ParserConfigurationException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        docBuilder = factory.newDocumentBuilder();
+    }
+    
+    public Element loadNode(String text) throws SAXException, IOException {
+        return docBuilder.parse(new InputSource(new StringReader(text))).getDocumentElement();
+    }
+    
+    public void doTest(String node1, String node2, Options options, boolean similar, boolean equal) throws SAXException, IOException {
+        NodeEqualityWrapper eq1 = new NodeEqualityWrapper(loadNode(node1), options);
+        NodeEqualityWrapper eq2 = new NodeEqualityWrapper(loadNode(node2), options);
+        
+        if(equal)
+            Assert.assertTrue(eq1.equals(eq2));
+        else
+            Assert.assertFalse(eq1.equals(eq2));
+
+        NodeSimilarityWrapper sim1 = new NodeSimilarityWrapper(loadNode(node1), options);
+        NodeSimilarityWrapper sim2 = new NodeSimilarityWrapper(loadNode(node2), options);
+        
+        if(similar)
+            Assert.assertTrue(sim1.equals(sim2));
+        else
+            Assert.assertFalse(sim1.equals(sim2));
+    }
+    
+    private static final Options OPTS_DEFAULT = new Options(false, true, true, false, false);
+    private static final Options OPTS_SIM_ATT = new Options(false, true, false, false, false);
+    private static final Options OPTS_EL_NO_ORDER = new Options(true, true, true, false, false);
+    private static final Options OPTS_ATT_ORDER = new Options(false, false, true, false, false);
+    
+    @Test
+    public void testEmptyElementVariants() throws Exception {
+        doTest("<element></element>", "<element/>", OPTS_DEFAULT, true, true);
+    }
+    
+    @Test
+    public void testAttributePresenceEffectOnSimilarity() throws Exception {
+        doTest("<element id=\"1\"></element>", "<element/>", OPTS_DEFAULT, true, false);
+        doTest("<element id=\"1\"></element>", "<element/>", OPTS_SIM_ATT, false, false);
+    }
+    
+    @Test
+    public void testAttributeDifferenceEffectOnSimilarity() throws Exception {
+        doTest("<element id=\"1\"></element>", "<element id=\"0\"/>", OPTS_DEFAULT, true, false);
+        doTest("<element id=\"1\"></element>", "<element id=\"0\"/>", OPTS_SIM_ATT, false, false);
+    }
+
+    @Test
+    public void testText() throws Exception {
+        doTest("<element>Text</element>", "<element>Text</element>", OPTS_DEFAULT, true, true);
+        doTest("<element>Text1</element>", "<element>Text2</element>", OPTS_DEFAULT, true, false);
+        doTest("<element>Text1<a/>Text2</element>", "<element>Text1<a/>Text2</element>", OPTS_DEFAULT, true, true);
+    }
+    
+    @Test
+    public void testPI() throws Exception {
+        doTest("<root><?target Data?></root>", "<root><?target \n   Data?></root>", OPTS_DEFAULT, true, true);
+        doTest("<root><?target Data?></root>", "<root><?target \n   OtherData?></root>", OPTS_DEFAULT, true, false);
+    }
+    
+    @Test
+    public void testComment() throws Exception {
+        doTest("<root><!--Comment--></root>", "<root><!--Comment--></root>", OPTS_DEFAULT, true, true);
+        doTest("<root><!--Comment--></root>", "<root><!-- Comment --></root>", OPTS_DEFAULT, true, false);
+        doTest("<root><!--Comm--><!--ent--></root>", "<root><!--Comment--></root>", OPTS_DEFAULT, true, false);
+        doTest("<root><!--Comm--><!--ent--></root>", "<root><!--Comm--><!--ent--></root>", OPTS_DEFAULT, true, true);
+    }
+    
+    @Test
+    public void testOrderOfElements() throws Exception {
+        doTest("<root><a/><b/></root>", "<root><a/><b/></root>", OPTS_DEFAULT, true, true);
+        doTest("<root><a/><b/></root>", "<root><a/><b/></root>", OPTS_EL_NO_ORDER, true, true);
+
+        doTest("<root><a/><b/></root>", "<root><b/><a/></root>", OPTS_DEFAULT, true, false);
+        doTest("<root><a/><b/></root>", "<root><b/><a/></root>", OPTS_EL_NO_ORDER, true, true);
+
+        doTest("<root><a/><b/><b/></root>", "<root><b/><a/><b/></root>", OPTS_EL_NO_ORDER, true, true);
+        doTest("<root><a/><b/><b/></root>", "<root><a/><b/><b/></root>", OPTS_EL_NO_ORDER, true, true);
+
+        doTest("<root><a/>x<b/>y</root>", "<root><a/>x<b/>y</root>", OPTS_EL_NO_ORDER, true, true);
+        doTest("<root><a/>x<b/>y</root>", "<root><b/>x<a/>y</root>", OPTS_EL_NO_ORDER, true, true);
+        doTest("<root><a/>x<b/>y</root>", "<root><b/>y<a/>x</root>", OPTS_EL_NO_ORDER, true, false);
+    }
+}
