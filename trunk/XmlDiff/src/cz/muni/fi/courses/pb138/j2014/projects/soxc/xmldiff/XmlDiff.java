48f5918af9a265e87c1c338a1097d54db11c8864
ondrej.sobocik@gmail.com <ondrej.sobocik@gmail.com@24eb42d8-d958-32f7-5ae2-b5831bfd505f>
2014-06-15 13:15:29 +0000

    modified cmd interface

--- trunk/XmlDiff/src/cz/muni/fi/courses/pb138/j2014/projects/soxc/xmldiff/XmlDiff.java
+++ trunk/XmlDiff/src/cz/muni/fi/courses/pb138/j2014/projects/soxc/xmldiff/XmlDiff.java
@@ -7,21 +7,26 @@
 package cz.muni.fi.courses.pb138.j2014.projects.soxc.xmldiff;
 
 import cz.muni.fi.courses.pb138.j2014.projects.soxc.Options;
+import cz.muni.fi.courses.pb138.j2014.projects.soxc.PreprocessingOptions;
 import cz.muni.fi.courses.pb138.j2014.projects.soxc.Soxc;
 import cz.muni.fi.courses.pb138.j2014.projects.soxc.consumers.FlatConsumers;
 import cz.muni.fi.courses.pb138.j2014.projects.soxc.consumers.FlatJustDocumentDiffConsumer;
+import cz.muni.fi.courses.pb138.j2014.projects.soxc.consumers.textoutput.TextOutputDiffConsumer;
+import cz.muni.fi.courses.pb138.j2014.projects.soxc.consumers.xmloutput.XmlOutputDiffConsumer;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLStreamException;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 /**
  * Main class of the XML diff command-line tool.
- * @author Jakub Kremlacek
- * @author Matej Minarik
- * @author Ondrej Mosnacek
  * @author Ondrej Sobocik
  */
 public class XmlDiff {
@@ -42,29 +47,56 @@ public class XmlDiff {
         
         String documentLeftURI;
         String documentRightURI;
-        FlatJustDocumentDiffConsumer consumer;
+        FlatJustDocumentDiffConsumer consumer = null;
+        File firstDocument = new File(args[0]);
+        File secondDocument = new File(args[1]);
+        
+        if(!firstDocument.exists()){
+            System.err.println("Missing first document URI.");
+            System.exit(2);
+            
+        }
+        if(!secondDocument.exists()){
+            System.err.println("Missing second document URI");
+            System.exit(2);
+        }             
+        
+        if(args[2].startsWith("-")){
+            System.err.println("Third argument must be output method.");
+            System.exit(2);
+        }
+        if(args[2]=="text"){
+            consumer = new TextOutputDiffConsumer();
+        }else{
+            try {
+                consumer = new XmlOutputDiffConsumer(new FileWriter(new File(args[2])));
+            } catch (Exception ex) {
+                System.err.println("Couldn't create output file "+args[2]);
+            }
+        }
         
         Options.Builder optBuilder = new Options.Builder();
-        
-        
-        String[] tmp = parseCommandLine(args, optBuilder);
-        documentLeftURI = tmp[0];
-        documentRightURI = tmp[1];
-        /*
-            according to the options the consumer will be either a TextOutputDiffConsumer
-            or an XmlOutputDiffConsumer (linked to a user-specified output file)
-        */
-        consumer = null;
-        // ...
-        
-        Options options = optBuilder.buildOptions();
+        PreprocessingOptions.Builder preBuilder = new PreprocessingOptions.Builder();
         
         // Create the document builder factory:
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        
         // Turn on namespace awareness:
         factory.setNamespaceAware(true);
         
+        parseCommandLine(args, optBuilder, preBuilder, factory);
+        
+        /*
+            according to the options the consumer will be either a TextOutputDiffConsumer
+            or an XmlOutputDiffConsumer (linked to a user-specified output file)
+            for now cmd output
+        */
+        //consumer = new TextOutputDiffConsumer();
+        // ...
+        
+        Options options = optBuilder.buildOptions();
+        PreprocessingOptions preproOptions = preBuilder.buildOptions();
+        
+           
         // Get the DocumentBuilder instance from the factory:
         DocumentBuilder builder;
         try {
@@ -77,26 +109,25 @@ public class XmlDiff {
         }
         
         // Load the documents into a DOM tree:
-        Document docL, docR;
+        Document docL = null;
+        Document docR = null;
         try {
-            docL = builder.parse(documentLeftURI);
+            docL = builder.parse(firstDocument);
         } catch (SAXException | IOException ex) {
             System.err.println("ERROR: Unable to load the left document:");
             System.err.println(ex);
             System.exit(2);
-            return;
         }
         try {
-            docR = builder.parse(documentRightURI);
+            docR = builder.parse(secondDocument);
         } catch (SAXException | IOException ex) {
             System.err.println("ERROR: Unable to load the right document:");
             System.err.println(ex);
             System.exit(2);
-            return;
         }
         
         // Compare the documents:
-        boolean result = Soxc.diffDocuments(docL, docR, options, FlatConsumers.toGeneral(consumer));
+        boolean result = Soxc.diffDocumentsPreprocess(docL, docR, options, preproOptions, FlatConsumers.toGeneral(consumer));
         
         System.exit(result ? 0 : 1);
     }
@@ -107,17 +138,13 @@ public class XmlDiff {
      * The function for parsing command-line arguments
      * @param args the command line arguments
      * @param optBuilder the optBuilder to set otions
-     * @return 2 Strings, left and right documents URI
      */
-    private static String[] parseCommandLine(String[] args, Options.Builder optBuilder){
-        String[] retval = new String[]{"",""};
-        int i=1;
-        String arg;
-        char flag;
-        while(i < args.length && args[i].startsWith("-")){
-            arg = args[i++];
+    private static void parseCommandLine(String[] args, Options.Builder optBuilder,
+            PreprocessingOptions.Builder preBuilder, DocumentBuilderFactory factory){
+        int i=3;
+        while(i < args.length && args[i].startsWith("-")){          
             //full arguments
-            switch (arg) {
+            switch (args[i]) {
                 case "-ignorePrefix":
                     optBuilder.setIgnorePrefix(true);
                     break;
@@ -142,41 +169,56 @@ public class XmlDiff {
                 case "-useAttributesInSimilarity":
                     optBuilder.setIgnoreAttributesInSimilarity(false);
                     break;
-                default:
-                    //flags argumensts
-                    for (int j = 1; j < arg.length(); j++) {
-                        flag = arg.charAt(j);
-                        switch (flag) {
-                            case 'p':
-                                optBuilder.setIgnorePrefix(true);
-                                break;
-                            case 'u':
-                                optBuilder.setIgnoreNamespaceURI(true);
-                                break;
-                            default:
-                                System.err.println("illegal option " + flag);
-                                break;
-                        }
-                    }  
-                break;
-            }
-        } 
-        if(i == args.length){
-            System.err.println("ERROR: missing left document URI");
-        } else {
-            i++;
-            retval[0] = args[i];
+                case "-ignoreElementNameInSimilarity":
+                    optBuilder.setIgnoreElementNameInSimilarity(true);
+                    break;
+                case "-useElementNameInSimilarity":
+                    optBuilder.setIgnoreElementNameInSimilarity(false);
+                    break;
+                //preprocesing options
+                case "-ignoreWhiteSpaceOnlyText":
+                    preBuilder.setIgnoreWhitespaceOnlyText(true);
+                    break;
+                case "-useWhiteSpaceOnlyText":
+                    preBuilder.setIgnoreWhitespaceOnlyText(true);
+                    break;
+                case "-trimWhiteSpaceInText":
+                    preBuilder.setTrimWhitespaceInText(true);
+                    break;
+                case "-doNotTrimWhiteSpaceInText":
+                    preBuilder.setTrimWhitespaceInText(false);
+                    break;    
+                case "-ignoreCDATA":
+                    preBuilder.setIgnoreCDATA(true);
+                    break;
+                case "-useCDATA":
+                    preBuilder.setIgnoreCDATA(false);
+                    break;
+                case "-ignoreTextData":
+                    preBuilder.setIgnoreText(true);
+                    break;
+                case "-useTextData":
+                    preBuilder.setIgnoreText(false);
+                    break;
+                case "-ignoreProcessingInstructions":
+                    preBuilder.setIgnoreProcessingInstructions(true);
+                    break;
+                case "-useProcessingInstructions":
+                    preBuilder.setIgnoreProcessingInstructions(false);
+                    break;
+                case "-ignoreComments":
+                    factory.setIgnoringComments(true);
+                    break;
+                default:                    
+                    System.err.println("illegal option " + args[i]);
+                    break;                        
+            }  
+            i++;             
         }
-        if(i == args.length){
-            System.err.println("ERROR: missing right document URI");
-        } else {
-            i++;
-            retval[1] = args[i];
-        }
-        return retval;
-    }
+    } 
+}  
     
     
-}
+
 
 
